c2f764f5a56878edb8a885138a4600483a5b4507
Saumya Palk <saumyp3@gmail.com>
2022-11-04 08:47:07 -0700

    new socket commits

--- src/main/script.js
+++ src/main/script.js
@@ -3,13 +3,18 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
+// send message + input (containers)
+
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
+// when user joins, connection altert
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
 })
+
+// append -- adds message + name each time a message is sent
 
 socket.on('user-connected', name => {
   appendMessage(`${name} connected`)
@@ -19,6 +24,8 @@ socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
 
+
+// user connection and disconnection 
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
@@ -26,6 +33,8 @@ messageForm.addEventListener('submit', e => {
   socket.emit('send-chat-message', message)
   messageInput.value = ''
 })
+
+// submit button to send
 
 function appendMessage(message) {
   const messageElement = document.createElement('div')
--- src/main/server.js
+++ src/main/server.js
@@ -1,18 +1,22 @@
 const io = require('socket.io')(8015)
-
+//connnects to port
 const users = {}
-
+//defines users
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
   })
+
+  //new user connected alert
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
   })
+  //allows message to be sent
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
+    //alert if user leaves
   })
 })
 
